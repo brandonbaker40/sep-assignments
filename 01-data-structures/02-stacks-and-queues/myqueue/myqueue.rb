ea041d336b6b893c58cafe4970386dc9d8f98fc7
Brandon Baker <brandonbaker40@icloud.com>
2018-01-13 16:22:29 -0700

    uses less basic methods than push and pop

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -9,13 +9,15 @@ class MyQueue
   end
 
   def enqueue(element)
-    @queue.push(element)
+    #@queue.push(element)
+    @queue.insert(@queue.length, element)
     @head = @queue[0]
     @tail = @queue[@queue.length - 1]
   end
 
   def dequeue
-    @queue.pop
+    #@queue.pop
+    @queue.delete(@queue.last)
     @head = @queue[0]
     @tail = @queue[@queue.length - 1]
   end
