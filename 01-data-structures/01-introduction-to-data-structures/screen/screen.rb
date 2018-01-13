4a62de1ad73417e3cad3830e34b59bc1fe737155
Brandon Baker <brandonbaker40@icloud.com>
2018-01-13 13:17:41 -0700

    completes assignment on intro to data structures

--- 01-data-structures/01-introduction-to-data-structures/screen/screen.rb
+++ 01-data-structures/01-introduction-to-data-structures/screen/screen.rb
@@ -6,18 +6,32 @@ class Screen
   attr_accessor :matrix
 
   def initialize(width, height)
+    @width = width
+    @height = height
+    @matrix = Array.new(height){Array.new(width)} # http://bit.ly/2AXkDU8
   end
 
   # Insert a Pixel at x, y
   def insert(pixel, x, y)
+    @matrix[x][y] = pixel
   end
 
   def at(x, y)
+    if inbounds(x, y)
+      @matrix[x][y]
+    else
+      nil
+    end
   end
 
   private
 
   def inbounds(x, y)
+    if (x >= 0 && x <= @width) && (y >= 0 && y <= @height)
+      return true
+    else
+      return false
+    end
   end
 
 end
