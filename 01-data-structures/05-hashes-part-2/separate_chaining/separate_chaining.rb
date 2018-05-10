8ef96500dcb781b6cd6296b131c7b67135e6d478
Brandon Baker <brandonbaker40@icloud.com>
2018-05-09 18:33:00 -0700

    fixes bug in resize object for separate chaining

--- 01-data-structures/05-hashes-part-2/separate_chaining/separate_chaining.rb
+++ 01-data-structures/05-hashes-part-2/separate_chaining/separate_chaining.rb
@@ -3,22 +3,13 @@ require_relative 'node'
 
 
 class SeparateChaining
-  attr_reader :max_load_factor, :nodes
+  attr_reader :max_load_factor, :nodes, :node_count
 
   def initialize(size)
     @max_load_factor = 0.7
     @nodes = Array.new(size)
     @node_count = 0
   end
-
-  # DEF INSERT(key, value)
-  #     COMPUTE the hash code for key, ASSIGN to index
-  #     GET array[index], ASSIGN to oldHead
-  #     SET array[index] to a link that contains key and value
-  #     IF oldHead exists THEN
-  #         SET oldHead as the second link in the chain
-  #     END IF
-  # END DEF
 
   def []=(key, value)
     node_index = index(key, size) # perform the index function and store it in a variable node_index
@@ -39,14 +30,12 @@ class SeparateChaining
   def [](key)
     node_index = index(key, size)
     current_node = @nodes[node_index].head
-    if @nodes[node_index].head
-      while current_node.next
+      while current_node
         if current_node.key == key
           return current_node.value
         end
         current_node = current_node.next
       end
-    end
     return
   end
 
@@ -59,7 +48,7 @@ class SeparateChaining
 
   # Calculate the current load factor
   def load_factor
-    @node_count / size.to_f # number of total items divded by the number of buckets
+    @node_count / size.to_f # number of total items divded by the number of linked_lists
   end
 
   # Simple method to return the number of items in the hash
@@ -67,39 +56,26 @@ class SeparateChaining
     @nodes.size # calculate the length of the array of items
   end
 
-  # def resize
-  #   new_size = @nodes.length * 2
-  #
-  #   new_array = Array.new(new_size)
-  #   #new_hash = SeparateChaining.new(new_size) # this is me creating an entirely new instance, which is bad
-  #   @nodes.each do |linked_list|
-  #     if @nodes[linked_list]
-  #         current_node = @nodes[linked_list].head
-  #         until current_node == nil do
-  #           new_index = current_node.key.sum % new_array.length
-  #           new_array[new_index] = LinkedList.new
-  #           new_array[new_index].add_to_front(current_node)
-  #           current_node = current_node.next
-  #         end
-  #         @nodes[linked_list] = nil
-  #       end
-  #     end
-  #   end
-
   def resize
-      old_nodes = @nodes
-      new_size = old_nodes.size * 2
-      @nodes = Array.new(new_size)
+    # make a copy of the old array
+    old_nodes = @nodes
+    # establish the new_size
+    new_size = old_nodes.length * 2
+    # makes an empty array of twice the number of buckets as the old hash
+    new_hash = SeparateChaining.new(new_size)
+    # compacts them first so we dont loop over empty lists
+    old_nodes.compact.each do |linked_list|
+      # start the current_node at the head and work down the list
+      current_node = linked_list.head
+      # while you have a current_node to perform on
+      while current_node
+        # re-insert the key, value pairs into the new hash
+        new_hash[current_node.key] = current_node.value
 
-      old_nodes.each do |bucket| # for each item in the old nodes, we need to transfer to the new @nodes
-        if bucket #!= nil
-          current_node = bucket.head
-          while current_node #!= nil
-            self.[]=(current_node.key, current_node.value)
-            current_node = current_node.next
-          end
-        end
+        current_node = current_node.next
       end
     end
+    @nodes = new_hash.nodes
+  end
 
 end
